df85334a7f27489b474375064f5b06fdcaafec39
Alish Shrestha <cresthaalish@gmail.com>
2025-01-20 11:36:32 +0545

    Update sitemaps to include tags

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,5 +1,6 @@
 from django.contrib.sitemaps import Sitemap
-
+from django.urls import reverse
+from taggit.models import Tag
 from .models import Post
 
 
@@ -15,3 +16,13 @@ class PostSitemap(Sitemap):
   def lastmod(self, obj):
     # receives each object returned by items() and returns the last time the object was modified
     return obj.updated
+  
+class TagSitemap(Sitemap):
+  changefreq = 'daily'
+  priority = 0.7
+
+  def items(self):
+    return Tag.objects.all()
+  
+  def location(self, obj):
+    return reverse('blog:post_list_by_tag', kwargs={'tag_slug': obj.slug})
